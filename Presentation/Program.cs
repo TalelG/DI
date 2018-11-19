c485bc80ef44a10e3f3b7749b2714b74d9929b90
tgara <tgara@EB-OR6281537>
2018-11-19 21:27:18 +0100

    Add project files.

---
+++ Presentation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Presentation
+{
+    //https://www.codeproject.com/Tips/1157736/Unity-Dependency-Injection-with-N-Layer-Project
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Bootstrapper.Init();
+            var initiator = DependencyInjector.Retrieve<Initiator>();
+            string data = initiator.FetchData();
+            Console.WriteLine(data);
+            Console.ReadLine();
+        }
+    }
+}
