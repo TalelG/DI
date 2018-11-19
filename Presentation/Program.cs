9ac1e242208801b3b43774429bf6c13490637223
tgara <tgara@EB-OR6281537>
2018-11-19 21:38:28 +0100

    [GT] Clean

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -5,11 +5,11 @@ namespace Presentation
     //https://www.codeproject.com/Tips/1157736/Unity-Dependency-Injection-with-N-Layer-Project
     public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Bootstrapper.Init();
             var initiator = DependencyInjector.Retrieve<Initiator>();
-            string data = initiator.FetchData();
+            var data = initiator.FetchData();
             Console.WriteLine(data);
             Console.ReadLine();
         }
